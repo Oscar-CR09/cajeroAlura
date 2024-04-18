70134d6cba627ba979dd5d0f8fa5cb187ef1f989
Oscar <oscarcr189@outlook.com>
2024-04-17 23:12:49 -0600

    "Cajero iniciando"

---
+++ src/Desafio.java
@@ -0,0 +1,13 @@
+public class Desafio {
+    public static void main(String[] args) {
+        String nombre ="Diana Llanos";
+        String tipoDeCuenta = "Corriente";
+        double saldo = 2599.99;
+
+        System.out.println("***************");
+        System.out.println("\nNombre del Cliente: " + nombre );
+        System.out.println("\nEl tipo de cuenta es: " + tipoDeCuenta);
+        System.out.println("Su saldo disponible es: " + saldo + "$");
+
+    }
+}
